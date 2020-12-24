25c73f98047d85cfcdd42927dcc8f0e5e5ee0b70
Dmitry Roar <dmitrydima1488@yandex.ru>
2020-12-25 00:39:16 +0700

    xz chto tut

--- client/src/containers/SwitchTheme/SwitchTheme.tsx
+++ client/src/containers/SwitchTheme/SwitchTheme.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classes from './SwtichTheme.module.scss'
+
 import {useDispatch} from 'react-redux'
 
 import {toggleTheme} from '../../store/actions/auth'
@@ -9,17 +11,36 @@ interface Props {
 }
 
 export const SwitchTheme: React.FC<Props> = ({theme}) => {
+  const [modal, setModal] = useState(true)
   const dispatch = useDispatch()
 
   const switchThemeHandler = () => {
     dispatch(toggleTheme(theme))
   }
 
+  const cls = [classes.Wrap, (!modal && classes.Close)]
+  theme === 'dark' ? cls.push(classes.Dark) : cls.push(classes.Light)
+  console.log(cls)
+
+  const closeSwithThemeHadnler = () => {
+    cls.includes(classes.Close) ? setModal(false) : setModal(true)
+    console.log(cls)
+  }
+
   return (
-    <div>
-      <button onClick={switchThemeHandler}>
-        {theme}
-      </button>
+    <div className={cls.join(' ')} >
+      <div className={classes.Wave} onClick={closeSwithThemeHadnler}>
+        <i className='fas fa-times' />
+      </div>
+      <div onClick={switchThemeHandler} className={classes.Icon}>
+        <div>
+          {
+            theme === 'dark'
+            ? <i className='far fa-moon' />
+            : <i className='fas fa-sun' />
+          }
+        </div>
+      </div>
     </div>
   )
 }
